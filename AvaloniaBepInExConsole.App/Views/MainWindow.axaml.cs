0dc1d5868733cc48ab2cd49101298a2ea0bd14ad
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-23 06:27:05 +0000

    make the main window reactive

--- AvaloniaBepInExConsole.App/Views/MainWindow.axaml.cs
+++ AvaloniaBepInExConsole.App/Views/MainWindow.axaml.cs
@@ -1,11 +1,14 @@
-using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using AvaloniaBepInExConsole.App.ViewModels;
+using ReactiveUI;
 
 namespace AvaloniaBepInExConsole.App.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
+        this.WhenActivated(_ => { });
         InitializeComponent();
     }
 }
