0493ecbafd3241d39008c59d2bec34095108ca74
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-23 06:45:48 +0000

    we do some coding in the view model

--- AvaloniaBepInExConsole.App/ViewModels/MainWindowViewModel.cs
+++ AvaloniaBepInExConsole.App/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,35 @@
-﻿namespace AvaloniaBepInExConsole.App.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using AvaloniaBepInExConsole.App.Logs;
+using DynamicData;
+using ReactiveUI;
 
-public class MainWindowViewModel : ViewModelBase
+namespace AvaloniaBepInExConsole.App.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, IActivatableViewModel
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public ViewModelActivator Activator { get; }
+
+    private ReadOnlyObservableCollection<LogMessage>? _logMessages;
+    public ReadOnlyObservableCollection<LogMessage>? LogMessages => _logMessages;
+
+    public ObservableCollection<LogMessage> TestLogMessages { get; } = new(Enumerable.Repeat(new LogMessage("the quick brown fox jumps over the lazy dog 0123456789"), 150));
+
+    public MainWindowViewModel()
+    {
+        Activator = new ViewModelActivator();
+
+        this.WhenActivated(disposables => {
+
+            var logListener = new BepInExLogListener()
+                .DisposeWith(disposables);
+
+            logListener.LogMessages.Connect()
+                .Bind(out _logMessages)
+                .Subscribe();
+
+        });
+    }
 }
