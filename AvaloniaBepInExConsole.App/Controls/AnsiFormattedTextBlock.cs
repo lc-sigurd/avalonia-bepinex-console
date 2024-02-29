76ff799b95b157dd3e7b47467ef8e426bef2d7c6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-29 11:19:24 +0000

    use reflection to access constraint (enables text wrapping)

--- AvaloniaBepInExConsole.App/Controls/AnsiFormattedTextBlock.cs
+++ AvaloniaBepInExConsole.App/Controls/AnsiFormattedTextBlock.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Reflection;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Media.TextFormatting;
@@ -7,6 +10,11 @@ namespace Sigurd.AvaloniaBepInExConsole.App.Controls;
 
 public class AnsiFormattedTextBlock : TextBlock
 {
+    private static FieldInfo _constraintFieldInfo = typeof(TextBlock).GetField("_constraint", bindingAttr: BindingFlags.Instance | BindingFlags.NonPublic)!;
+    protected override Type StyleKeyOverride => typeof(TextBlock);
+
+    protected Size Constraint => (Size)_constraintFieldInfo.GetValue(this)!;
+
     protected override TextLayout CreateTextLayout(string? text)
     {
         var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
@@ -26,6 +34,8 @@ public class AnsiFormattedTextBlock : TextBlock
             textSource,
             paragraphProperties,
             TextTrimming,
+            Constraint.Width,
+            Constraint.Height,
             maxLines: MaxLines);
     }
 }
