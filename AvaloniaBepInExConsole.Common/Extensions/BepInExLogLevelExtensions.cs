b8e2474be6f24183d0be33a50e5c4173fa528d91
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-29 08:10:45 +0000

    embolden error and fatal messages

--- AvaloniaBepInExConsole.Common/Extensions/BepInExLogLevelExtensions.cs
+++ AvaloniaBepInExConsole.Common/Extensions/BepInExLogLevelExtensions.cs
@@ -8,8 +8,8 @@ public static class BepInExLogLevelExtensions
             BepInExLogLevel.Info => "\x1b[0;38;5;7m",
             BepInExLogLevel.Message => "\x1b[0;38;5;15m",
             BepInExLogLevel.Warning => "\x1b[0;38;5;11m",
-            BepInExLogLevel.Error => "\x1b[0;38;5;1m",
-            BepInExLogLevel.Fatal => "\x1b[0;38;5;9m",
+            BepInExLogLevel.Error => "\x1b[0;1;38;5;1m",
+            BepInExLogLevel.Fatal => "\x1b[0;1;38;5;9m",
             _ => "\x1b[0m"
         };
 }
