750ded98e977d59b52611a2cde6695b40eb51373
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-29 12:11:22 +0000

    reduce threshold for IsScrolledToEnd

--- AvaloniaBepInExConsole.App/Extensions/ScrollChangedEventArgsExtensions.cs
+++ AvaloniaBepInExConsole.App/Extensions/ScrollChangedEventArgsExtensions.cs
@@ -4,6 +4,6 @@ namespace Sigurd.AvaloniaBepInExConsole.App.Extensions;
 
 internal static class ScrollChangedEventArgsExtensions
 {
-    public static bool IsScrolledToEnd(this ScrollChangedEventArgs args) => args.OffsetDelta.Y + args.ViewportDelta.Y > args.ExtentDelta.Y - 7.0;
+    public static bool IsScrolledToEnd(this ScrollChangedEventArgs args) => args.OffsetDelta.Y + args.ViewportDelta.Y > args.ExtentDelta.Y - 1.0;
     public static bool DidScrollUp(this ScrollChangedEventArgs args) => args.OffsetDelta.Y < 0;
 }
