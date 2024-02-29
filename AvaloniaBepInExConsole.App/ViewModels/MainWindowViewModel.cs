b5f5b7121e928bf1d3ef77efa8fd064d57467b10
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-29 12:10:11 +0000

    remove `TestLogMessages`

--- AvaloniaBepInExConsole.App/ViewModels/MainWindowViewModel.cs
+++ AvaloniaBepInExConsole.App/ViewModels/MainWindowViewModel.cs
@@ -12,8 +12,6 @@ public class MainWindowViewModel : ViewModelBase
     private readonly ReadOnlyObservableCollection<LogEvent> _logMessages;
     public ReadOnlyObservableCollection<LogEvent> LogMessages => _logMessages;
 
-    public ObservableCollection<LogEvent> TestLogMessages { get; } = new(Enumerable.Repeat(new LogEvent { Data = "\x1b[0;38;5;99mthe quick bro\x1b[0mwn fox jumps over \x1b[107;94mthe lazy dog 0123456789", Level = BepInExLogLevel.Info, SourceName = "Foobar"}, 150));
-
     public MainWindowViewModel(ILogListener logListener)
     {
         logListener.LogMessages.Connect()
