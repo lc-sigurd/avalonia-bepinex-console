169eff1bc30bcf9c6180990703d528d3338b4936
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-29 12:09:44 +0000

    add dummy log listener

---
+++ AvaloniaBepInExConsole.App/Logs/DummyLogListener.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DynamicData;
+using Microsoft.Extensions.Hosting;
+using Sigurd.AvaloniaBepInExConsole.Common;
+
+namespace Sigurd.AvaloniaBepInExConsole.App.Logs;
+
+public class DummyLogListener : BackgroundService, ILogListener
+{
+    public SourceList<LogEvent> LogMessages { get; } = new();
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        LogMessages.AddRange(Enumerable.Repeat(TestLogEvent, 20));
+        while (!stoppingToken.IsCancellationRequested) {
+            await Task.Delay(750, stoppingToken);
+            LogMessages.Add(TestLogEvent);
+        }
+    }
+
+    private static LogEvent TestLogEvent => new LogEvent { Data = "\x1b[0;38;5;99mthe quick bro\x1b[0mwn fox\n jumps\n over \x1b[107;94mthe \nlazy dog 0123456\n789", Level = BepInExLogLevel.Info, SourceName = "Foobar" };
+}
