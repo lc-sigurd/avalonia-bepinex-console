e27dec353e5953bf97626daa3d1e50ca3cf551f5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-03 14:14:58 +0000

    get ansi formatted content when implemented

--- AvaloniaBepInExConsole.Server/Extensions/LogEventArgsExtensions.cs
+++ AvaloniaBepInExConsole.Server/Extensions/LogEventArgsExtensions.cs
@@ -7,8 +7,17 @@ public static class LogEventArgsExtensions
 {
     public static LogEvent ToAvaloniaBepInExConsoleLogEvent(this LogEventArgs logEventArgs)
         => new LogEvent() {
-            Content = logEventArgs.ToString(),
+            Content = logEventArgs.GetContent(),
             Level = logEventArgs.Level.ToAvaloniaBepInExConsoleLogLevel(),
             SourceName = logEventArgs.Source.SourceName,
         };
+
+    private static string GetContent(this LogEventArgs logEventArgs)
+    {
+        if (logEventArgs is IAnsiFormattable ansiFormattable) {
+            return ansiFormattable.ToAnsiFormattedString();
+        }
+
+        return logEventArgs.ToString();
+    }
 }
