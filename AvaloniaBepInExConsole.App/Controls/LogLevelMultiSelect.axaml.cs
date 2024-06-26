362c26a3ceb8dc3dc2fe7bcece5a4b4fd3252072
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-26 02:30:57 +0100

    move log level select to a `UserControl`

---
+++ AvaloniaBepInExConsole.App/Controls/LogLevelMultiSelect.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Sigurd.AvaloniaBepInExConsole.App.Controls;
+
+public partial class LogLevelMultiSelect : UserControl
+{
+    public LogLevelMultiSelect()
+    {
+        InitializeComponent();
+    }
+
+    private void LogLevelSelectChanged(object? sender, RoutedEventArgs e)
+    {
+
+    }
+}
