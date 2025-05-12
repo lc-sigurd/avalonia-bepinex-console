9b1dc508350f401ce6897e42e7abed79a8b9df24
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-12 15:10:38 +0100

    add executable permission + discern correct `java` path

--- libs/aeron-media-driver/start-media-driver.sh
+++ libs/aeron-media-driver/start-media-driver.sh
@@ -3,6 +3,9 @@
 # https://stackoverflow.com/a/77663806/11045433
 AeronDriverDirectory=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v -- "$0" )"}" )")
 
+# https://serverfault.com/a/1100799
+Java=$(update-alternatives --list java | grep "java-8")
+
 echo Media Driver Started...
-java -cp "$AeronDriverDirectory/media-driver.jar" io.aeron.driver.MediaDriver
+$Java -cp "$AeronDriverDirectory/media-driver.jar" io.aeron.driver.MediaDriver
 echo Media Driver Stopped.
