60294b07e047255445e1b5f709ec4f26d5c2245d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-12 15:45:14 +0100

    support specifying path to aeron directory

--- libs/aeron-media-driver/start-media-driver.sh
+++ libs/aeron-media-driver/start-media-driver.sh
@@ -6,6 +6,9 @@ AeronDriverDirectory=$(dirname "$( readlink -f "${BASH_SOURCE[0]:-"$( command -v
 # https://serverfault.com/a/1100799
 Java=$(update-alternatives --list java | grep "java-8")
 
+# 1st positional parameter: aeron directory. Defaults to /dev/shm/aeron-USER
+AeronDir=${1:-"/dev/shm/aeron-$(whoami)"}
+
 echo Media Driver Started...
-$Java -cp "$AeronDriverDirectory/media-driver.jar" io.aeron.driver.MediaDriver
+$Java -cp "$AeronDriverDirectory/media-driver.jar" -Daeron.dir="$AeronDir" io.aeron.driver.MediaDriver
 echo Media Driver Stopped.
